37d1dc074587764b0585f22b31190a59411c1fd0
GrigoryGriko <griko1996@gmail.com>
2024-03-06 23:28:20 +0300

    first commit

---
+++ src/components/Test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div className="Test">
+      <header className="est-header">
+        <p>Компонент реакта</p>
+      </header>
+    </div>
+  );
+}
+
+export default Test;
