cbc8e683cd528ce8c2822a77860e9c8b65a3287b
GrigoryGriko <griko1996@gmail.com>
2024-03-06 23:33:56 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+
+function test(a: number, b: number): string {
+  return 'result: ' + a + b;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Реакт работает
+          Реакт работает. <br></br>
+
+          {test(5, 8)}
         </p>
       </header>
     </div>
